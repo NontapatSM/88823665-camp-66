10823fb8c8b377938c3cb2f77e67cd9ac181c0dc
NontapatSM <nontapat.sinthum@gmail.com>
2025-01-13 13:26:00 +0700

    laravel
    สูตรคูณ

--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -1,11 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Mycontroller;
+
+Route::get('/Mycontroller', [Mycontroller::class, 'myfunction']);
+
+Route::post('/Mycontroller', [Mycontroller::class, 'myfunction']);
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/Hello', function () {
-    return "<h1>Hello World!</h1>";
+Route::get('/Hello', function ($val="") {
+    return "<h1>Hello World! </h1>";
 });
